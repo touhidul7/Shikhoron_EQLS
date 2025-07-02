2a7b9223c061b7d77f3f80fefae7784f400c322a
touhidul-islam-html <ti360109@gmail.com>
2025-07-03 02:47:53 +0600

    working

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: backendUrl + '/api',
   withCredentials: true
 });
 
